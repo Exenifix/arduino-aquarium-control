022d0b60129e0d8be33ca82bc1980d00a42f1cbc
Exenifix <89513380+Exenifix@users.noreply.github.com>
2025-10-27 21:42:19 +0300

    Add servo + adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <TinyIRSender.hpp>
+#include <Servo.h>
 
 // Thermistor constants
 #define THERM_PIN A0
@@ -21,13 +22,30 @@
 #define IR_CMD_ON 0x7
 #define IR_CMD_SUNRISE 0xD
 #define IR_CMD_NOON 0x15
-#define IR_CMD_EVENING 0xB
+#define IR_CMD_EVENING 0x11
 #define IR_CMD_NEON 0x16
+
+// Servo constants
+#define SERVO_PIN 6
+#define SERVO_CALIB (-3)
+#define SERVO_DEGREE 30
+#define SERVO_DELAY_MS 250
 
 // Other constants
 #define MIN_OPTIMAL_TEMP 23
 #define MAX_OPTIMAL_TEMP 27
 #define IR_SYNC_INTERVAL 10000
+#define FISH_FEED_HOUR 17
+#define FEED_BUTTON_PIN 4
+static const int PROGMEM color_progress[][2] {
+    {-2, 8},
+    {8, 10},
+    {10, 12},
+    {12, 18},
+    {18, 20},
+    {20, 22},
+    {22, 32}
+};
 
 // Bitmaps
 #define WARNING_BITMAP_H 16
@@ -41,6 +59,7 @@ static const unsigned char PROGMEM warning_bitmap[] = {
 GyverNTC therm(THERM_PIN, THERM_RES, THERM_BETA);
 RTC_DS3231 rtc;
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Servo servo;
 
 // Mutable variables
 float temp;
@@ -99,6 +118,23 @@ uint8_t get_current_color() {
     return IR_CMD_OFF;
 }
 
+float get_current_color_progress() {
+    int v1 = 0, v2 = 24;
+    int a, b;
+    for (unsigned int i = 0; i < sizeof color_progress / sizeof color_progress[0]; i++) {
+        a = pgm_read_word(&color_progress[i][0]);
+        b = pgm_read_word(&color_progress[i][1]);
+        if (hour < b) {
+            v1 = a;
+            v2 = b;
+            break;
+        }
+    }
+    v1 *= 60;
+    v2 *= 60;
+    return constrain((float) (hour * 60 + minute - v1) / (v2 - v1), 0.f, 1.f);
+}
+
 const __FlashStringHelper *get_current_color_name() {
     switch (current_color) {
         case IR_CMD_OFF:
@@ -121,6 +157,10 @@ void gather_data() {
     update_datetime();
 }
 
+void set_servo_degree(const int degree) {
+    servo.write(degree + SERVO_CALIB);
+}
+
 void update_ir() {
     if (millis() - last_ir_update < IR_SYNC_INTERVAL) {
         return;
@@ -128,12 +168,9 @@ void update_ir() {
     last_ir_update = millis();
 
     auto new_color = get_current_color();
-    if (current_color == new_color) {
-        return;
-    }
     if (current_color == IR_CMD_OFF) {
         send_ir_command(IR_CMD_ON);
-        delay(100); // will mess up a blink for one time, but it's acceptable
+        delay(300); // will mess up a blink for one time, but it's acceptable
     }
     send_ir_command(new_color);
     current_color = new_color;
@@ -174,10 +211,29 @@ void update_display() {
     display.print(F("C"));
 
     // current lights status
-    display.setCursor(57, 55);
+    display.setCursor(57, 53);
     display.print(get_current_color_name());
 
+    // color progress
+    display.drawFastHLine(0, 63, static_cast<int16_t>(get_current_color_progress() * 128), WHITE);
+
     display.display();
+}
+
+void feed_fish() {
+    set_servo_degree(120 + SERVO_DEGREE);
+    delay(SERVO_DELAY_MS);
+    set_servo_degree(120);
+}
+
+void check_feeder() {
+    if (rtc.alarmFired(1)) {
+        rtc.clearAlarm(1);
+        feed_fish();
+    }
+    if (digitalRead(FEED_BUTTON_PIN)) {
+        feed_fish();
+    }
 }
 
 void setup_rtc() {
@@ -190,6 +246,9 @@ void setup_rtc() {
 
     if (rtc.lostPower()) {
         Serial.println(F("RTC lost power, setting time to compile datetime"));
+        rtc.disableAlarm(2);
+        rtc.clearAlarm(1);
+        rtc.setAlarm1(DateTime(2025, 1, 1, FISH_FEED_HOUR), DS3231_A1_Hour);
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
 
@@ -205,10 +264,23 @@ void setup_display() {
     Serial.println(F("[OK] Display initialized"));
 }
 
+void setup_servo() {
+    Serial.println(F("Initializing servo"));
+    servo.attach(SERVO_PIN);
+    if (!servo.attached()) {
+        Serial.println(F("Failed to initialize servo"));
+        halt();
+    }
+    set_servo_degree(120);
+    Serial.println(F("[OK] Servo initialized"));
+}
+
 void setup() {
     Serial.begin(9600);
+    pinMode(FEED_BUTTON_PIN, INPUT);
     setup_rtc();
     setup_display();
+    setup_servo();
     last_ir_update = millis();
 }
 
@@ -216,5 +288,6 @@ void loop() {
     gather_data();
     update_ir();
     update_display();
+    check_feeder();
     delay(1000);
 }
